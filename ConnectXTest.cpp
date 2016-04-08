c12a733fe6d3a900aa23bc1a437a6f41447baffd
priyanka <pchordia@mail.csuchico.edu>
2016-04-08 10:07:21 -0700

    Connect four game test cases and bugs

--- ConnectXTest.cpp
+++ ConnectXTest.cpp
@@ -19,3 +19,121 @@ TEST(ConnectXTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+// The bug is in this test case as the width is negative and so the test case fails.
+// The output returned shows it to be valid.
+// But the expected output is -1.
+
+TEST(ConnectXTest, inBoundsWidth)
+{
+	ConnectX connect(5,5,3);
+	connect.placePiece(3);
+
+	ASSERT_EQ(-1, connect.at(-4,4));
+}
+
+TEST(ConnectXTest, inBoundsHeight)
+{
+	ConnectX connect(5,5,3);
+
+	ASSERT_EQ(-1, connect.at(4,-4));
+}
+
+TEST(ConnectXTest, inBoundsWidthHeight)
+{
+	ConnectX connect(5,5,3);
+
+	ASSERT_EQ(-1, connect.at(-4,-4));
+}
+
+TEST(ConnectXTest, inBoundsWidthHeightPositive)
+{
+	ConnectX connect(5,5,3);
+
+	ASSERT_EQ(0, connect.at(4,4));
+}
+
+// The bug is in this test case as the width is out of bounds and so the test case fails.
+// The output returned shows it to be valid.
+// But the expected output is -1.
+
+TEST(ConnectXTest, outBoundsWidth)
+{
+	ConnectX connect(5,5,3);
+	
+	ASSERT_EQ(-1, connect.at(7,4));
+}
+
+TEST(ConnectXTest, outBoundsHeight)
+{
+	ConnectX connect(5,5,3);
+	
+	ASSERT_EQ(-1, connect.at(3,7));
+}
+
+TEST(ConnectXTest, outBoundsWidthHeight)
+{
+	ConnectX connect(5,5,3);
+	
+	ASSERT_EQ(-1, connect.at(7,7));
+}
+
+// Constructor with negative width 
+TEST(ConnectXTest, widthTurn)
+{
+	ConnectX connect(-5,5,3);
+	
+	ASSERT_EQ(2, connect.whoseTurn());
+}
+
+// Constructor with negative height 
+TEST(ConnectXTest, heightTurn)
+{
+	ConnectX connect(5,-5,3);
+	
+	ASSERT_EQ(2, connect.whoseTurn());
+}
+
+// Constructor with negative x
+TEST(ConnectXTest, xTurn)
+{
+	ConnectX connect(5,5,-3);
+	
+	ASSERT_EQ(2, connect.whoseTurn());
+}
+
+TEST(ConnectXTest, outOfBoundsWidth)
+{
+	ConnectX connect(0,5,3);
+	
+	ASSERT_EQ(2, connect.whoseTurn());
+}
+
+TEST(ConnectXTest, outOfBoundsHeight)
+{
+	ConnectX connect(5,0,3);
+	
+	ASSERT_EQ(2, connect.whoseTurn());
+}
+
+TEST(ConnectXTest, outOfBoundsX)
+{
+	ConnectX connect(5,5,0);	
+	ASSERT_EQ(2, connect.whoseTurn());
+}
+
+TEST(ConnectXTest, checkPiece)
+{
+	ConnectX connect(1,1,3);
+	connect.placePiece(1);
+	ASSERT_EQ(1, connect.whoseTurn());
+}
+
+TEST(ConnectXTest, checkPiecePlaceAndBoard)
+{
+	ConnectX connect(5,5,3);
+	connect.placePiece(3);
+	connect.placePiece(4);
+	connect.showBoard();
+	ASSERT_EQ(2, connect.whoseTurn());
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+* Priyanka Chordia
+
+Bugs Found:
+
+The bug is caused due to inBounds(int w, int h) function.
+
+1. TEST(ConnectXTest, inBoundsWidth)
+** Bug Description:** The bug occurs in the test for checking the inBound value of Width. When a negative value of width is passed to the at() function, the output that is expected is INVALID. But the output that we get shows this condition is valid, as it checks the value of width first and it gets inside = FALSE. Next it checks the value of height and as it is positive the value of inside changes to inside = TRUE. Thus returning the entire value of the function as TRUE that shows that it is a valid output. Hence the condition should be checked properly to obtain the correct output and handle the case of negative Width passed with positive Height.
+
+2. TEST(ConnectXTest, outBoundsWidth)
+** Bug Description:** The bug occurs in the test for checking the outBound value of Width. When a larger value of width which is not in the range is passed to the at() function, the output that is expected is INVALID. But the output that we get shows this condition is valid, as it checks the value of width first and it gets inside = FALSE. Next it checks the value of height and as it is in the defined range the value of inside changes to inside = TRUE. Thus returning the entire value of the function as TRUE that shows that it is a valid output. Hence the condition should be checked properly to obtain the correct output and handle the case of larger value of Width which is out of the range passed with the value of Height which is in the defined range.
